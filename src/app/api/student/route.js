7777445c625c54d294fda4e0a2271f4259cff877
Kalyan Devara <kalyan.vza@gmail.com>
2024-07-09 12:28:31 +0530

    updated

---
+++ src/app/api/student/route.js
@@ -0,0 +1,16 @@
+import { DBconnection } from "@/app/utils/config/db";
+import { NextResponse } from "next/server";
+
+
+const ConnectDB = async()=>{
+        await DBconnection()
+}
+
+ConnectDB()
+
+
+export async function GET(){
+    return NextResponse.json({student:"all student data"})
+}
+
+console.log(process.env)
