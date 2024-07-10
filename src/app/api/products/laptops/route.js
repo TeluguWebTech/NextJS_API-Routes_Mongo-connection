956bf0b887d93568481fae8fd979a7fc867a3b1e
Kalyan Devara <kalyan.vza@gmail.com>
2024-07-10 23:25:45 +0530

    crud-operations updated

--- src/app/api/products/laptops/route.js
+++ src/app/api/products/laptops/route.js
@@ -2,7 +2,6 @@ import { DBconnection } from "@/app/utils/config/db";
 import LaptopModel from "@/app/utils/models/Laptop";
 import { NextResponse } from "next/server";
 
-
 const ConnectDB = async()=>{
     await DBconnection()
 }
@@ -22,4 +21,19 @@ export async function POST(request){
         })
 
         return NextResponse.json({msg:"Laptop added successfully"})
+}
+
+
+export async function PUT(request){
+        
+    const laptopId = await request.nextUrl.searchParams.get("id");
+    const {newTitle:name, newModel: laptopmodel, newPrice:laptopprice} = await request.json()
+    await LaptopModel.findByIdAndUpdate(laptopId, {name, laptopmodel, laptopprice});
+    return NextResponse.json({msg:"Laptop Product Updated"})
+}
+
+export async function DELETE(request){
+    const laptopId = await request.nextUrl.searchParams.get("id");
+    await LaptopModel.findByIdAndDelete(laptopId)
+    return NextResponse.json({msg:'laptop product deleted successfully'})
 }
--- src/app/api/products/mobiles/route.js
+++ src/app/api/products/mobiles/route.js
@@ -2,13 +2,11 @@ import { DBconnection } from "@/app/utils/config/db";
 import MobileModel from "@/app/utils/models/Mobile";
 import { NextResponse } from "next/server";
 
-
 const ConnectDB = async()=>{
         await DBconnection()
 }
 
 ConnectDB()
-
 
 export async function GET(){
         const mobileData = await MobileModel.find({})
@@ -23,3 +21,20 @@ export async function POST(request){
 
     return NextResponse.json({success: "Mobile added Successfully!"})
 }
+
+
+export async function PUT(request){
+        
+        const mobileId = await request.nextUrl.searchParams.get("id");
+        const {newTitle:title, newModel: model, newPrice:price} = await request.json()
+        await MobileModel.findByIdAndUpdate(mobileId, {title, model, price});
+        return NextResponse.json({msg:"Mobile Product Updated"})
+}
+
+export async function DELETE(request){
+        const mobileId = await request.nextUrl.searchParams.get("id");
+        await MobileModel.findByIdAndDelete(mobileId)
+        return NextResponse.json({msg:'Mobile product deleted successfully'})
+    }
+    
+
